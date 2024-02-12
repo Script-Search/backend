31414758c3e7cf1b6536558e493e11cd8e0be051
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-02-12 10:35:54 -0600

    barebones api

---
+++ transcript_api/main.py
@@ -0,0 +1,24 @@
+import functions_framework
+
+@functions_framework.http
+def transcript_api(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and "request" in request_json:
+        request = request_json["request"]
+    elif request_args and "request" in request_args:
+        request = request_args["request"]
+    else:
+        request = "World"
+    return str({"request": str(request)})
+
