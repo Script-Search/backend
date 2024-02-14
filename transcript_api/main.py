14568838c26ff6011a62e9e4eac219202bcb3513
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-02-14 11:44:27 -0600

    firebase query in the transcript api

--- transcript_api/main.py
+++ transcript_api/main.py
@@ -1,16 +1,28 @@
 import functions_framework
 from flask import jsonify, Request
+from firebase_admin import credentials, firestore, initialize_app
+from typing import Dict, Any
+
+
+def get_transcript(video_id: str) -> Dict[str, Any]:
+    cred = credentials.Certificate("credentials.json")
+    initialize_app(cred)
+    db = firestore.client()
+    test = db.collection("test")
+    document = test.document(video_id).get()
+    return document.to_dict()
+
 
 @functions_framework.http
 def transcript_api(request: Request) -> Request:
     """HTTP Cloud Function for handling transcript requests.
-    
+
     This function handles incoming HTTP requests containing transcript data.
-    
+
     Args:
         request (flask.Request): The request object.
             <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    
+
     Returns:
         Tuple containing:
         - The response text, or any set of values that can be turned into a
@@ -19,6 +31,7 @@ def transcript_api(request: Request) -> Request:
         - HTTP status code.
         - Headers for the response.
     """
+
     request_json = request.get_json(silent=True)
     request_args = request.args
     headers = {
@@ -32,7 +45,7 @@ def transcript_api(request: Request) -> Request:
         request = request_args["request"]
     else:
         request = None
-    
+
     data = {
         "request": request
     }
