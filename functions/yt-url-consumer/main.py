61924f5127aaee114b4cdf3385485becd203ddf1
DNCHOW1 <dienchau@LAPTOP-IF9S7856>
2024-02-14 12:38:54 -0600

    small change to test ci/cd

--- functions/yt-url-consumer/main.py
+++ functions/yt-url-consumer/main.py
@@ -5,4 +5,5 @@ import functions_framework
 @functions_framework.cloud_event
 def transcript_downloader(cloud_event):
     # Print out the data from Pub/Sub, to prove that it worked
+    print("Test CI/CD")
     print(base64.b64decode(cloud_event.data["message"]["data"]))
