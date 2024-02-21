7e4fd8f18984f1ff8eb6110e5bfb34f4e6aa9476
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-02-21 14:06:28 -0600

    remove deploy.yml

--- functions/yt-url-consumer/main.py
+++ functions/yt-url-consumer/main.py
@@ -94,8 +94,7 @@ def insert_transcript(ttml_file_name):
 @functions_framework.cloud_event
 def transcript_downloader(cloud_event):
     startTime = time.time()
-    # URL = base64.b64decode(cloud_event.data["message"]["data"]).decode("utf-8")
-    URL = cloud_event.data
+    URL = base64.b64decode(cloud_event.data["message"]["data"]).decode("utf-8")
 
     if "watch" not in URL: # TODO: Ensure inputted URL is a singular video
         return 200
