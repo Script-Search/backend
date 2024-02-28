a1fdc11fb134d07f771db039bddae51a9ababc6a
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-02-28 10:28:09 -0600

    add limit to number of videos

--- transcript_api/playlist.py
+++ transcript_api/playlist.py
@@ -1,26 +1,22 @@
 import yt_dlp
 
-limit = 250
-ydl_opts = {
-        'quiet': True,
-        'extract_flat': True,
-        'extractor_args': {
-            'youtube': {
-                'playlistend': limit
-                }
-            }
+LIMIT = 250
+YDL_OPS = {
+        "quiet": True,
+        "extract_flat": True,
+        "playlist_items": f"1-{LIMIT}",
         }
 
-ydl = yt_dlp.YoutubeDL(ydl_opts)
+YDL = yt_dlp.YoutubeDL(YDL_OPS)
 
 def get_playlist_videos(playlist_url):
-    playlist = ydl.extract_info(playlist_url, download=False)
+    playlist = YDL.extract_info(playlist_url, download=False)
     video_urls = [ entry["url"] for entry in playlist["entries"] ]
 
     return video_urls
 
 def get_channel_videos(channel_url):
-    channel = ydl.extract_info(channel_url, download=False)
+    channel = YDL.extract_info(channel_url, download=False)
     video_urls = [ entry["url"] for entry in channel["entries"][0]["entries"] ]
 
     return video_urls
