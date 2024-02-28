7219b0db396ff0893e6cf5a2813062912e14738b
BJ <byjong@tamu.edu>
2024-02-28 12:16:21 -0600

    use regex to determine if a link is a video

--- transcript_api/playlist.py
+++ transcript_api/playlist.py
@@ -1,4 +1,7 @@
+import re
 import yt_dlp
+from typing import List
+
 
 LIMIT = 250
 YDL_OPS = {
@@ -8,6 +11,38 @@ YDL_OPS = {
         }
 
 YDL = yt_dlp.YoutubeDL(YDL_OPS)
+VALID_VIDEO = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu\.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+VALID_PLAYLIST = r"^.*(youtu.be\/|list=)([^#\&\?]*).*"
+
+def process_url(url: str) -> List[str]:
+    """
+    Takes a Universal Reference Link, determines if the url is a channel or a playlist and returns NUMBER most recent videos.
+
+    Args:
+        url (str): Universsal Reference Link
+
+    Returns:
+        List[str]: list of video urls
+    """
+    is_short = "short" in url
+    is_feed = "feed" in url
+
+    if is_short or is_feed:
+        raise ValueError(f"This type of url is not supported")
+
+    is_video = re.search(VALID_VIDEO, url)
+    is_playlist = "list=" in url
+    is_channel = "channel" in url or "@" in url
+
+    if is_video:
+        print("Video")
+    elif is_playlist:
+        print("Playlist")
+    elif is_channel:
+        print("Channel")
+    else:
+        print("Invalid")
+    return
 
 def get_playlist_videos(playlist_url):
     playlist = YDL.extract_info(playlist_url, download=False)
@@ -22,14 +57,10 @@ def get_channel_videos(channel_url):
     return video_urls
 
 def main():
-    # playlist_url = "https://youtube.com/playlist?list=PL94lfiY18_CgWGQzweD_aVjsFXiRi6kn5&si=UmiI8Ou09GHXwo6n"
-    channel_url = "https://www.youtube.com/@PewDiePie"
-    # video_urls = get_playlist_videos(playlist_url)
-    video_urls = get_channel_videos(channel_url)
-
-    for url in video_urls:
-        print(url)
-    print(len(video_urls))
+    url = "https://youtube.com/playlist?list=PL94lfiY18_CgWGQzweD_aVjsFXiRi6kn5&si=UmiI8Ou09GHXwo6n"
+    # url = "https://www.youtube.com/watch?v=xlQ0psr7Th4&list=PL94lfiY18_CgWGQzweD_aVjsFXiRi6kn5"
+    # url = "https://www.youtube.com/@PewDiePie"
+    process_url(url)
 
 if __name__ == "__main__":
     main()
