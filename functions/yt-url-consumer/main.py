3edc4df8fec904c23a654a3e212e3b28d9b7fd0b
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-03-10 20:42:54 -0500

    add comments

--- functions/yt-url-consumer/main.py
+++ functions/yt-url-consumer/main.py
@@ -7,31 +7,71 @@ import yt_dlp
 
 from firebase_admin import credentials, firestore, initialize_app
 from lxml import etree
+from Typing import Tuple, List
 
-delimiter = "$#$"
+test_collection = None
 
-ydl_opts = {
+DELIMITER = "$#$"
+"""
+Delimiter used to separate the different fields in the output file name
+"""
+
+YDL_OPTS = {
     "skip_download": True,
     "writesubtitles": True,
     "writeautomaticsub": True,
-    "subtitleslangs": ["en"], # TODO: Handle other languages (en-.*)
+    "subtitleslangs": ["en"],  # TODO: Handle other languages (en-.*)
     "subtitlesformat": "ttml",
-    'outtmpl': {'default': delimiter.join(["%(id)s", "%(channel_id)s", "%(uploader)s", "%(duration)s", "%(upload_date)s", "%(title)s"])}, # Change output file name
-    "quiet": True, # Don't display stuff to the console
+    "outtmpl": {
+        "default": DELIMITER.join(["%(id)s", "%(channel_id)s", "%(uploader)s", "%(duration)s", "%(upload_date)s", "%(title)s"])
+    },
+    "quiet": True, 
 }
+"""
+Options for yt-dlp to download the subtitles in the right format
+"""
 
-test_collection = None
-ydl = yt_dlp.YoutubeDL(ydl_opts)
+YDL = yt_dlp.YoutubeDL(YDL_OPTS)
+"""
+yt-dlp object to download the subtitles
+"""
 
-def is_valid_subtitle(element):
+
+def is_valid_subtitle(element: etree._Element) -> bool:
+    """Checks if the element is a valid subtitle element
+
+    Args:
+        element (idk): The element to check
+    """
+
     return element.tag.endswith('p') and 'begin' in element.attrib and 'end' in element.attrib and element.text
 
-def parse_timestamp(timestamp):
+
+def parse_timestamp(timestamp: str) -> int:
+    """Parse the timestamp into seconds
+
+    Args:
+        timestamp (str): The timestamp in the format HH:MM:SS.sss
+
+    Returns:
+        int: The timestamp in seconds
+    """
+    
     hours, minutes, seconds = timestamp.split(":")
     seconds = seconds.split(".")[0]
     return int(hours) * 3600 + int(minutes) * 60 + int(seconds)
 
-def parse_ttml(ttml_file_name):
+
+def parse_ttml(ttml_file_name: str) -> Tupe[List[str], List[int]]:
+    """Parse the ttml file into a list of subtitles and a list of timestamps
+
+    Args:
+        ttml_file_name (str): The name of the ttml file to parse
+
+    Returns:
+        Tuple[List[str], List[int]]: A tuple containing the list of subtitles and the list of timestamps
+    """
+    
     subtitles = []
     timestamps = []
 
@@ -42,18 +82,16 @@ def parse_ttml(ttml_file_name):
                 text = element.text.strip()
                 begin = element.attrib['begin']
                 subtitles.append(text)
-                timestamps.append(parse_timestamp(begin)) # TODO: convert begin to seconds
-
-            # Clear element to release memory, can help w/ mem usage
-            # element.clear()
-
-            # # Clear parent element to release memory, can help w/ mem usage
-            # while element.getprevious() is not None:
-            #     del element.getparent()[0]
+                # TODO: convert begin to seconds
+                timestamps.append(parse_timestamp(begin))
 
     return subtitles, timestamps
 
-def initialize_firestore(): # Using lazy-loading of global variable, more efficient for serverless functions
+
+def initialize_firestore() -> None:
+    """Uses lazy-loading to initialize the firestore client and collection
+    """
+
     global test_collection
     if test_collection == None:
         cred = credentials.Certificate("credentials.json")
@@ -61,12 +99,23 @@ def initialize_firestore(): # Using lazy-loading of global variable, more effici
         db = firestore.client()
         test_collection = db.collection("test")
 
-def insert_transcript(ttml_file_name):
+
+def insert_transcript(ttml_file_name: str) -> bool:
+    """Inserts the transcript data into the firestore database
+
+    Args:
+        ttml_file_name (str): The name of the ttml file to insert
+
+    Returns:
+        bool: True if successful, False otherwise
+    """
+
     try:
         initialize_firestore()
 
         # TODO: Handle potential error where there isn't exactly 1 ttml file
-        video_id, channel_id, channel_name, duration, upload_date, title = ttml_file_name.split(delimiter)
+        video_id, channel_id, channel_name, duration, upload_date, title = ttml_file_name.split(
+            DELIMITER)
 
         # Convert to the right data format (according to wiki)
         duration = int(duration)
@@ -80,7 +129,7 @@ def insert_transcript(ttml_file_name):
             "channel_id": channel_id,
             "channel_name": channel_name,
             "duration": int(duration),
-            "upload_date": upload_date, # Maybe defer this to a later function
+            "upload_date": upload_date,  # Maybe defer this to a later function
             "title": title,
             "transcript": parsed_transcript,
             "timestamps": timestamps
@@ -91,12 +140,23 @@ def insert_transcript(ttml_file_name):
         return False
 
 # Triggered from a message on a Cloud Pub/Sub topic.
+
+
 @functions_framework.cloud_event
-def transcript_downloader(cloud_event):
+def transcript_downloader(cloud_event: functions_framework.CloudEvent) -> None:
+    """Downloads the transcript from the given URL and inserts it into the firestore database
+
+    Args:
+        cloud_event (CloudEvent): The cloud event containing the URL to download the transcript from
+
+    Returns:
+        None
+    """
+    
     startTime = time.time()
     URL = base64.b64decode(cloud_event.data["message"]["data"]).decode("utf-8")
 
-    if "watch" not in URL: # TODO: Ensure inputted URL is a singular video
+    if "watch" not in URL:  # TODO: Ensure inputted URL is a singular video
         print("watch not in URL")
         return 400
 
@@ -108,7 +168,8 @@ def transcript_downloader(cloud_event):
     #         - In case of multiple function calls this avoids annoying edge cases
     # '''
     ttml_files = glob.glob("*.ttml")
-    ttml_file_name = ttml_files[0] if len(ttml_files) == 1 else None # There could be more if function called for multiple different URLs
+    # There could be more if function called for multiple different URLs
+    ttml_file_name = ttml_files[0] if len(ttml_files) == 1 else None
 
     if not ttml_file_name:
         print("No ttml files found")
@@ -116,5 +177,6 @@ def transcript_downloader(cloud_event):
 
     insert_transcript(ttml_file_name)
 
-    os.remove(ttml_file_name) # Cleanup the ttml file to prepare the function for another URL; prevent memory increase
+    # Cleanup the ttml file to prepare the function for another URL; prevent memory increase
+    os.remove(ttml_file_name)
     print(f"Elapsed Time: {time.time() - startTime}")
