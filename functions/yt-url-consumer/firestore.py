0ad0e2f5d8c0d17793b00e980f6ada97e92ef9f9
Dien Chau <dienkchau@gmail.com>
2024-03-30 00:40:59 -0500

    Fixed singleton, added more error handling

--- functions/yt-url-consumer/firestore.py
+++ functions/yt-url-consumer/firestore.py
@@ -1,4 +1,5 @@
-from firebase_admin import credentials, firestore, initialize_app
+import firebase_admin
+from firebase_admin import credentials, firestore, initialize_app, get_app
 
 test_collection = None
 
@@ -6,10 +7,14 @@ def initialize_firestore() -> None:
     """Uses lazy-loading to initialize the firestore client and collection
     """
     global test_collection
-    if test_collection == None:
-        cred = credentials.Certificate("credentials.json")
-        initialize_app(cred)
-        db = firestore.client()
+    if test_collection is None:
+        if not firebase_admin._apps:
+            cred = credentials.Certificate("credentials.json")
+            app = initialize_app(cred)
+        else:
+            app = get_app()
+        
+        db = firestore.client(app=app)  # Use the retrieved or initialized app
         test_collection = db.collection("test")
 
 def upsert(document):
