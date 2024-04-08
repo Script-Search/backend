f5405d0d096159865357dead8d33a5eb955fc9c2
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-04-08 11:34:39 -0500

    update tests

--- transcript_api/test.py
+++ transcript_api/test.py
@@ -2,20 +2,9 @@ from unittest import TestCase, main
 from unittest.mock import patch
 
 from helpers import distribute
-from scrape import URLType, YDL_CLIENT, init_ydl_client, get_url_type, get_channel_videos, get_playlist_videos, get_video, process_url
-from search import TYPESENSE_CLIENT, init_typesense, search_typesense, search_playlist, mark_word
-from settings import MAX_VIDEO_LIMIT, TYPESENSE_SEARCH_PARAMS, TYPESENSE_SEARCH_REQUESTS
-
-class TestInitialize(TestCase):
-    @patch('scrape.init_ydl_client')
-    def test_init_ydl_client(self, mock_type):
-        init_ydl_client()
-        self.assertIsNotNone(YDL_CLIENT)
-
-    @patch('search.init_typesense')
-    def test_init_typesense(self, mock_type):
-        init_typesense()
-        self.assertIsNotNone(TYPESENSE_CLIENT)
+from scrape import *
+from search import * 
+from settings import *
 
 class TestGetURLType(TestCase):
     @patch('scrape.get_url_type')
@@ -138,6 +127,14 @@ class TestHelpers(TestCase):
         self.assertEqual(len(result[3]), 194//5 + 1)
         self.assertEqual(len(result[4]), 194//5)
 
+class TestMarkWord(TestCase):
+    @patch('search.mark_word')
+    def test_mark_word(self, mocktype):
+        word = "game"
+        text = "This is a game"
+        result = mark_word(text, word)
+        self.assertEqual(result, "This is a <mark>game</mark>")
+
 class TestSearch(TestCase):
     @patch('search.search_typesense')
     def test_search_no_filter(self, mocktype):
@@ -169,7 +166,22 @@ class TestSearch(TestCase):
         self.assertTrue("title" in result[0])
         self.assertTrue("channel_name" in result[0])
         self.assertTrue("matches" in result[0])
+
+    @patch('search.search_typesense')
+    def test_search_filter_playlist(self, mocktype):
+        mocktype.return_value = dict
+        copy_search_params = TYPESENSE_SEARCH_PARAMS.copy()
+        copy_search_params["q"] = "\"game\""
+        copy_search_params["video_ids"] = '["0baCL9wwJTA","RRP_GFSGrDA","gOFrQs_13mQ","dXBapNjnab4","StSGz5dx52s","kAT_xX-Xk6c","YlQ53oOQPRo","-pb7MrRnPv0","-pVg8ZnSARo","mVTbjsQbG1w","e5e1z3A3cfo","3-LJtJKYXrA","B_NscD2IECQ","DzioXvXd5fE","VUQs9prUia0","T0LYsNXkUX0","cz9ATZecggQ","2WKfYPAP5DQ","EYiil5yOHjQ","D1jTOSrwpOQ","9xgtuRqBS5A","mjfpixY7WdE","ecf7vPw8A2o","m9-mg4m6IW4","7vEXqbPCgao","RTSMxUsh3-g","HKx_VfcPJlU","Y1xzL8I9sAE","X0J2Lq2z2Vk","i__ZXVjCl_I","0JhTJRl4uew","X0xRJRF-g5w","ZaA7CqEK5Cc","QP3zjbr_U28","4u9KktKMLUg","1gfCG4bYVYM","npxaoLRfeXA","Y2_CN0StX5U","-UyZeZAeXPw","j1-D08FV780","AVFlqoQjh1M","2w_NQXb2KX4","3UC9qVNtCag","hQX0_MoBe9s","HP_BwlBgLb4","HYGIfWNe4NE","sZ6hp3keToo","R6rf2RPAe7s","aynVOIjCXvU","a5XKyiaw97c","Bb4dhojfB0o","RO0yTly_fqo","ivxXd__504o","bTbmOAbmrUk","kyW1Mo_uCDg","KhHIBFgaMCc","vlhsz5flDs4","4y_c2aCQ9hY","eACSzFJpetI","8_8BSdyEwoM","ZYSu2xg5c5w","qK1oN9dBs5A","89ZycCcH7sc","JU4qoXp3Sec","qjrwwV-bMTI","ujO3XtcPZ_4","LJBsRgSaVLU","i61Wcnjq-0Q","Lshh01K5JyI","zHACgmQkw3Q","qbJhi5Y0pLE","Z47RnEMnniQ"]'
     
+        result = search_typesense(copy_search_params)
+        self.assertIsNotNone(result)
+        self.assertTrue("video_id" in result[0])
+        self.assertTrue("channel_id" in result[0])
+        self.assertTrue("title" in result[0])
+        self.assertTrue("channel_name" in result[0])
+        self.assertTrue("matches" in result[0])
+
     @patch('search.search_playlist')
     def test_search_playlist(self, mocktype):
         playlist_url = r"https://www.youtube.com/playlist?list=PLBRObSmbZluRiGDWMKtOTJiLy3q0zIfd7"
@@ -195,8 +207,8 @@ class TestSearch(TestCase):
             sub_search["q"] = f"{query}"
             sub_search["filter_by"] = f"video_id:[{string_ids[i]}]"
 
-        print(copy_search_requests)
         result = search_playlist(copy_search_requests, copy_search_param)
+        self.assertIsNotNone(result)
 
 if __name__ == '__main__':
     main()
