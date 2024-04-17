a852b154d325f6b6cba63c248731ddec666893c3
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-04-17 10:34:44 -0500

    update unit tests

--- transcript_api/test.py
+++ transcript_api/test.py
@@ -137,7 +137,14 @@ class TestMarkWord(TestCase):
 
 class TestSearch(TestCase):
     @patch('search.search_typesense')
-    def test_search_no_filter(self, mocktype):
+    def test_search_failure(self, mocktype):
+        copy_search_params = TYPESENSE_SEARCH_PARAMS.copy()
+        copy_search_params["q"] = "the quick brown fox jumps over the lazy dog"
+
+        self.assertRaises(ValueError, search_typesense, copy_search_params)
+
+    @patch('search.search_typesense')
+    def test_search_single_no_filter(self, mocktype):
         mocktype.return_value = dict
         copy_search_params = TYPESENSE_SEARCH_PARAMS.copy()
         copy_search_params["q"] = "game"
@@ -153,7 +160,21 @@ class TestSearch(TestCase):
         self.assertIsNotNone(result[0]["matches"])
 
     @patch('search.search_typesense')
-    def test_search_filter_channel(self, mocktype):
+    def test_search_phrase_no_filter(self, mocktype):
+        mocktype.return_value = dict
+        copy_search_params = TYPESENSE_SEARCH_PARAMS.copy()
+        copy_search_params["q"] = "dynamic programming"
+
+        result = search_typesense(copy_search_params)
+        self.assertIsNotNone(result)
+        self.assertTrue("video_id" in result[0])
+        self.assertTrue("channel_id" in result[0])
+        self.assertTrue("title" in result[0])
+        self.assertTrue("channel_name" in result[0])
+        self.assertTrue("matches" in result[0])
+
+    @patch('search.search_typesense')
+    def test_search_single_filter_channel(self, mocktype):
         mocktype.return_value = dict
         copy_search_params = TYPESENSE_SEARCH_PARAMS.copy()
         copy_search_params["q"] = "game"
@@ -168,12 +189,42 @@ class TestSearch(TestCase):
         self.assertTrue("matches" in result[0])
 
     @patch('search.search_typesense')
-    def test_search_filter_playlist(self, mocktype):
+    def test_search_phrase_filter_channel(self, mocktype):
+        mocktype.return_value = dict
+        copy_search_params = TYPESENSE_SEARCH_PARAMS.copy()
+        copy_search_params["q"] = "dynamic programming"
+        copy_search_params["filter_by"] = "channel_id:UC_mYaQAE6-71rjSN6CeCA-g"
+
+        result = search_typesense(copy_search_params)
+        self.assertIsNotNone(result)
+        self.assertTrue("video_id" in result[0])
+        self.assertTrue("channel_id" in result[0])
+        self.assertTrue("title" in result[0])
+        self.assertTrue("channel_name" in result[0])
+        self.assertTrue("matches" in result[0])
+
+    @patch('search.search_typesense')
+    def test_search_single_filter_playlist(self, mocktype):
         mocktype.return_value = dict
         copy_search_params = TYPESENSE_SEARCH_PARAMS.copy()
         copy_search_params["q"] = "game"
         copy_search_params["video_ids"] = '["0baCL9wwJTA","RRP_GFSGrDA","gOFrQs_13mQ","dXBapNjnab4","StSGz5dx52s","kAT_xX-Xk6c","YlQ53oOQPRo","-pb7MrRnPv0","-pVg8ZnSARo","mVTbjsQbG1w","e5e1z3A3cfo","3-LJtJKYXrA","B_NscD2IECQ","DzioXvXd5fE","VUQs9prUia0","T0LYsNXkUX0","cz9ATZecggQ","2WKfYPAP5DQ","EYiil5yOHjQ","D1jTOSrwpOQ","9xgtuRqBS5A","mjfpixY7WdE","ecf7vPw8A2o","m9-mg4m6IW4","7vEXqbPCgao","RTSMxUsh3-g","HKx_VfcPJlU","Y1xzL8I9sAE","X0J2Lq2z2Vk","i__ZXVjCl_I","0JhTJRl4uew","X0xRJRF-g5w","ZaA7CqEK5Cc","QP3zjbr_U28","4u9KktKMLUg","1gfCG4bYVYM","npxaoLRfeXA","Y2_CN0StX5U","-UyZeZAeXPw","j1-D08FV780","AVFlqoQjh1M","2w_NQXb2KX4","3UC9qVNtCag","hQX0_MoBe9s","HP_BwlBgLb4","HYGIfWNe4NE","sZ6hp3keToo","R6rf2RPAe7s","aynVOIjCXvU","a5XKyiaw97c","Bb4dhojfB0o","RO0yTly_fqo","ivxXd__504o","bTbmOAbmrUk","kyW1Mo_uCDg","KhHIBFgaMCc","vlhsz5flDs4","4y_c2aCQ9hY","eACSzFJpetI","8_8BSdyEwoM","ZYSu2xg5c5w","qK1oN9dBs5A","89ZycCcH7sc","JU4qoXp3Sec","qjrwwV-bMTI","ujO3XtcPZ_4","LJBsRgSaVLU","i61Wcnjq-0Q","Lshh01K5JyI","zHACgmQkw3Q","qbJhi5Y0pLE","Z47RnEMnniQ"]'
     
+        result = search_typesense(copy_search_params)
+        self.assertIsNotNone(result)
+        self.assertTrue("video_id" in result[0])
+        self.assertTrue("channel_id" in result[0])
+        self.assertTrue("title" in result[0])
+        self.assertTrue("channel_name" in result[0])
+        self.assertTrue("matches" in result[0])
+
+    @patch('search.search_playlist')
+    def test_search_phrase_filter_playlist(self, mocktype):
+        mocktype.return_value = dict
+        copy_search_params = TYPESENSE_SEARCH_PARAMS.copy()
+        copy_search_params["q"] = "dynamic programming"
+        copy_search_params["video_ids"] = '["KLlXCFG5TnA", "1pkOgXD63yU", "3OamzN90kPg", "bNvIQI2wAjk", "5WZl3MMT0Eg", "lXVy6YWFcRM", "nIVW4P8b1VA", "U8XENwh8Oy8", "jzZsG8n2R9A", "UuiTKBwPgAo", "5Km3utixwZs", "RyBM56RIWrM", "WnPLSRLSANE", "UcoN6UjAI64", "Y0lT9Fck7qI", "H9bfqozjoqs", "cjWnW0hdF1Y", "Ua0GhsJSlWM", "Sx9NNgInc3A", "GBKI9VSKdGg", "73r3KWiEvyk", "rWAJCfYYOvM", "6aEyTjOwlJU", "IlEsdxuD4lY", "Yan0cv2cLy8", "mQeF6bN8hMk", "EgI5nU9etnU", "s-VkcjHqkGI", "pV2kpPD66nE", "P6RZZMu_maU", "6kTZYvNNyps", "bXsUuownnoQ", "8f1XPm4WOUc", "A8NUOmlwOlM", "44H3cEC2fFM", "nONCGxWoUfM", "PaJxqZVPhbg", "FdzJmTCVyJU", "G0_I-ZF0S38", "gBTe7lFR3vc", "XIdigk956u0", "q5a5OiGbT6Q", "XVuQxVej6y8", "S5bfdUTrKLM", "T41rL0L3Pnw", "BJnMZNwUk1M", "fMSJSS7eO1w", "pfiQ_PS1g8E", "wiGpQwVHdE0", "gqXU1UyA8pk", "jSto0O4AJbM", "vzdNOK2oB2E", "WTzjTskDFMg", "jJXJ16kPFWg", "XYQecbcd6_c", "4RACzI5-du8", "B1k_sxOSgv8", "hTM3phVI6YQ", "vRbbcKXCxOw", "OnSn2XEQ4MY", "Hr5cWUld4vU", "6ZnyEApgFYg", "u4JAi2JJhI8", "E36O5SWp-LE", "ihj4IQGZ2zc", "s6ATEkipzow", "5LUXSvjmGCw", "gs2LMfuOR9k", "oobqoCJlHA0", "BTf05gs_8iU", "asbcE9mZz_U", "YPTqKIgVk-k", "itmhHWaHupI", "9UtInBqnCgA", "gVUrDV4tZfY"]'
+
         result = search_typesense(copy_search_params)
         self.assertIsNotNone(result)
         self.assertTrue("video_id" in result[0])
